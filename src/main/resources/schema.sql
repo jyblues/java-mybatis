3b80d977f2fb442a2da139b5d7d2734284d6cc73
jyblues <jyblues@gmail.com>
2023-09-08 12:57:48 +0900

    feat: /api/v1/new.user 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,18 @@
 drop table if exists mybatis2.users;
 
-create or replace table mybatis2.users
+create table mybatis2.users
 (
-    id           varchar(20) not null primary key,
-    user_id      varchar(20) not null,
-    user_nm      varchar(50) null,
-    password     varchar(200) null,
-    access_token varchar(200) null,
-    email        varchar(50) null,
-    phone        varchar(30) null,
+    id           bigint auto_increment,
+    name         varchar(50)                          null,
+    password     varchar(200)                         null,
+    access_token varchar(200)                         null,
+    email        varchar(50)                          not null
+        primary key,
+    phone        varchar(30)                          null,
     create_at    datetime default current_timestamp() null,
     updated_at   datetime default current_timestamp() null,
-    deleted_at   datetime default current_timestamp() null
+    deleted_at   datetime default current_timestamp() null,
+    constraint users_id_email_name_uindex
+        unique (id, email, name)
 );
-
-create or replace index users_user_id_index
-    on mybatis2.users (user_id);
 
